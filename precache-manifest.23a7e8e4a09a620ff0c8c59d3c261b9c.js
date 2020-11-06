b24ca1df04c1b3a6924408c29ef16232ec062209
OlgaG <olga.goncharuk@ibsscorp.com>
2020-11-05 22:45:13 -0500

    Updates

---
+++ precache-manifest.23a7e8e4a09a620ff0c8c59d3c261b9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2fa30677c13c46697dea10b57758011",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f8ef0ec96adc2e9d70d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7f423c78d82403b8ad35",
+    "url": "/static/js/2.69f92635.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.69f92635.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f8ef0ec96adc2e9d70d",
+    "url": "/static/js/main.c62d5f5f.chunk.js"
+  },
+  {
+    "revision": "256c29fe58cebb602c58",
+    "url": "/static/js/runtime-main.351058ef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
